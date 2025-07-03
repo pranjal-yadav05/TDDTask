007b2f8d55f20622651acbdd5e5bf8ea5b3c1d89
pranjal-yadav05 <pranjalyadavpy212005@gmail.com>
2025-07-03 21:25:52 +0530

    add function fixed for single number

--- StringCalculatorTDD/src/main/java/org/StringCalculatorTDD/StringCalculator.java
+++ StringCalculatorTDD/src/main/java/org/StringCalculatorTDD/StringCalculator.java
@@ -2,6 +2,7 @@ package org.StringCalculatorTDD;
 
 public class StringCalculator {
     public int add(String numbers) {
-        return 0;
+        if (numbers.isEmpty()) return 0;
+        return Integer.parseInt(numbers);  // handles single number input
     }
 }
