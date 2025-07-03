25ac10f4de8ffaa09e9238a585da68e31f5612ae
pranjal-yadav05 <pranjalyadavpy212005@gmail.com>
2025-07-03 21:22:54 +0530

    Initial Commit with base

---
+++ StringCalculatorTDD/src/main/java/org/StringCalculatorTDD/StringCalculator.java
@@ -0,0 +1,7 @@
+package org.StringCalculatorTDD;
+
+public class StringCalculator {
+    public int add(String numbers) {
+        return 0;
+    }
+}
---
+++ StringCalculatorTDD/src/test/java/StringCalculatorTest.java
@@ -0,0 +1,12 @@
+import org.StringCalculatorTDD.StringCalculator;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StringCalculatorTest {
+
+    @Test
+    public void testEmptyStringReturnsZero() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(0, calculator.add(""));
+    }
+}
